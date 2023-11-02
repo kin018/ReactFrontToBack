120be9d2f6ab4c09aa8cf40eee5bed968d7da0c5
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-02 18:50:49 -0400

    removed example code for events

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -7,17 +7,11 @@ function FeedbackItem() {
   const [text, setText] = useState(
     'This is an example of a feedback item jawn'
   );
-  const handleClick = () => {
-    setRating((previous) => {
-      return previous + 1;
-    });
-  };
 
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
