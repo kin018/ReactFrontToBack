420832855acec5f95a8d250381a5cb679798cabb
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-02 15:52:33 -0400

    created feedbackitem component that are associated with the global css file as well as imported to app.js.  This contains hardcode temp values

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,11 +1,14 @@
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
+//state is simply just data
+//component level state--data assocaited to that one component and app level state--used in multiple components
 
 function App() {
   return (
     <>
       <Header />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackItem></FeedbackItem>
       </div>
     </>
   );
