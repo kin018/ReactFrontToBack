4337095510679ff2bd6c9dfe7615a16b2d4663b3
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-01 22:07:56 -0400

    Created components folder as well as Header component within

---
+++ feedback-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+//used rfce to generate this initial code snippet(ES7 React/Redux/GraphQL/React-Native snippets)
+import React from 'react';
+
+function Header() {
+  return <div></div>;
+}
+
+export default Header;
