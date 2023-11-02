e4f8dc56cd2ed7199556ebf1bf8a42c104b43f5b
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-02 12:39:47 -0400

    Adding style created var

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,6 +1,7 @@
 function Header({ text }) {
+  const headerStyles = { backgroundColor: 'blue', color: 'red' }; //this or in the style prop we would do this instead {{    const headerStyles ={ backgroundColor: 'blue', color: 'red' }}}
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -11,4 +12,5 @@ function Header({ text }) {
 Header.defaultProps = {
   text: 'Feedback UI',
 };
+
 export default Header;
