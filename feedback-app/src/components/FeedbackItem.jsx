31662267b6e8d24f927367d2ac3b72741dbb927f
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-02 18:10:52 -0400

    created States

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 function FeedbackItem() {
+  //[NAMEOFSTATE, FUNC]
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState(
+    'This is an example of a feedback item jawn'
+  );
+
   return (
     <div className="card">
-      <div className="num-display">10</div>
-      <div className="text-display">This is an example of a feedback item</div>
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
     </div>
   );
 }
