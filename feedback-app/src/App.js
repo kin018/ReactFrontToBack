79665ba3456fc7b3a4e105c250a8916fbd25200d
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-01 20:25:01 -0400

    updated files

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,6 +1,10 @@
 //components are either classes or functions
 function App() {
-  return <h1>Hello from the App component</h1>;
+  return (
+    <div className="container">
+      <h1>My App</h1>n{' '}
+    </div>
+  );
 }
 
 export default App;
--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -1,3 +1,18 @@
+//THIS IS FOR REACT 18 I will correct this after following tutorial
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// const root = document.getElementById('root');
+// const rootElement = (
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// ReactDOM.createRoot(root).render(rootElement);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
