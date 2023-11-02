d238e86491e4ca38701c1aa6eae2100eeb34fddb
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-01 22:12:43 -0400

    used rfce to generate this initial code snippet(ES7 React/Redux/GraphQL/React-Native snippets) can use _rfce to generate same code except this does not have import React statement since it is not neccessary. I have removed the import statement for consistency with tutorial

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,5 +1,6 @@
 //used rfce to generate this initial code snippet(ES7 React/Redux/GraphQL/React-Native snippets)
-import React from 'react';
+//can use _rfce to generate same code except this does not have import React statement since it is not neccessary.
+//I have removed the import statement for consistency with tutorial
 
 function Header() {
   return <div></div>;
