ff82e3b7e14c6a143c5c318b92f57e738b497902
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-07 11:12:49 -0500

    Changed FeedbackItem to FeedbackList in app.js

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
 import FeedbackData from './data/FeedbackData';
+import FeedbackList from './components/FeedbackList';
 
 function App() {
   //[NAMEOFSTATE, FUNCTION]
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList />
       </div>
     </>
   );
---
+++ feedback-app/src/components/FeedbackList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FeedbackList() {
+  return <div>List</div>;
+}
+
+export default FeedbackList;
