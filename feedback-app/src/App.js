5a9b88d5e7f94e261b91da6e0106f18d968c6626
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-07 10:08:38 -0500

    Created dummy data for global app component in data folder.  exported  and imported it to the app.js and currently using in the useState hook

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
-//state is simply just data
-//component level state--data assocaited to that one component and app level state--used in multiple components
+import FeedbackData from './data/FeedbackData';
 
 function App() {
-  const [feedback, setFeedback] = useState();
+  //[NAMEOFSTATE, FUNCTION]
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 2,
+    text: 'There were some issues with the product. I think it needs more refinement.',
+  },
+  {
+    id: 2,
+    rating: 5,
+    text: 'My experience was good overall, but there are areas that could be improved.',
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: "The service provided was exceptional. I couldn't be happier with it.",
+  },
+];
+
+export default FeedbackData;
