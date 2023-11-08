6cbc8bfc2368f6d11a4b96f74980b450adde3b0c
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-08 18:59:45 -0500

    Calculation for avg rating as well as neccessary html to display

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function FeedbackStats() {
-  return <div>Stats</div>;
+function FeedbackStats({ feedback }) {
+  let totalRating = feedback.reduce((acc, curr) => acc + curr.rating, 0);
+  let avgRating = totalRating / feedback.length;
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {avgRating}</h4>
+    </div>
+  );
 }
 
 export default FeedbackStats;
