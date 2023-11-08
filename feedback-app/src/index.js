9ba057b740711f5eea8b51d3cd5322345e060f2d
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-08 11:14:20 -0500

    Correct ReactDom.Render unsupported bug

--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -1,27 +1,12 @@
-//THIS IS FOR REACT 18 I will correct this after following tutorial
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-
-// const root = document.getElementById('root');
-// const rootElement = (
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// ReactDOM.createRoot(root).render(rootElement);
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-//.render(input, where in html it goes)
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
