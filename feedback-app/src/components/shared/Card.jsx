107687a532176ba84e3aa56d59c75385fc08f83a
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-08 10:48:44 -0500

    Conditional Style for Card example code

--- feedback-app/src/components/shared/Card.jsx
+++ feedback-app/src/components/shared/Card.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function Card({ children, reverse }) {
-  //conditional class for dark card and white text.  Notice the syntax reverse class is only shown if reverse prop is true
-  return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
+  // //conditional class for dark card and white text.  Notice the syntax reverse class is only shown if reverse prop is true
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
+
+  //in contrast to above this is a conditional style
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Card;
