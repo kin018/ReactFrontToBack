0d5f9de6c54ab49385c25471a432c0fd16fe1d01
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-07 19:42:28 -0500

    Completed managing Global state section

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import FeedbackItem from './FeedbackItem';
 
 function FeedbackList({ feedback }) {
-  console.log(feedback);
-  return <div>List</div>;
+  if (!feedback || feedback.length === 0) {
+    return <p>No FeedBack</p>;
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} /> //item is the prop name and {item} comes from feeback.map item param
+      ))}
+    </div>
+  );
 }
 
 export default FeedbackList;
