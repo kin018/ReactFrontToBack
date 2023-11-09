3a4c48d083b849b083e302ee6c6a08d33a829635
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-08 19:47:08 -0500

    Updates to form including a todo for later and functionality for button

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import Card from './shared/Card';
 
 function FeedbackForm() {
-  return <Card>FORM</Card>;
+  return (
+    <Card>
+      <form>
+        <h2>"Please rate your experience with us."</h2>
+        {/*@todo-Rating Select Component */}
+        <div className="input-group">
+          <input type="text" placeholder="Share your thoughts with a review." />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
