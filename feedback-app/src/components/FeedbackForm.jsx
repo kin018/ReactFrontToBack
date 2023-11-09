c7db4e23dae43889e0a1444033b855381ce6f16c
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-08 20:09:34 -0500

    used State for text and setText to forms target value

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import React from 'react';
 import Card from './shared/Card';
 
 function FeedbackForm() {
+  const [text, setText] = useState('');
+  const handleTextChange = (eventParam) => {
+    setText(eventParam.target.value);
+  };
+
   return (
     <Card>
       <form>
         <h2>"Please rate your experience with us."</h2>
         {/*@todo-Rating Select Component */}
         <div className="input-group">
-          <input type="text" placeholder="Share your thoughts with a review." />
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Share your thoughts with a review."
+            value={text}
+          />
           <button type="submit">Submit</button>
         </div>
       </form>
