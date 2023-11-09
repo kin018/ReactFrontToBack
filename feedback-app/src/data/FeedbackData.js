fca67b8be7c4bf156eff76f20141e696df7d9c22
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-08 19:01:51 -0500

    More Dummy Data

--- feedback-app/src/data/FeedbackData.js
+++ feedback-app/src/data/FeedbackData.js
@@ -19,6 +19,27 @@ const FeedbackData = [
     rating: 9,
     text: 'I found the overall experience to be quite satisfying. Will recommend it to others.',
   },
+  {
+    id: 5,
+    rating: 7,
+    text: 'The product met my expectations, but there is room for enhancement in its features.',
+  },
+  {
+    id: 6,
+    rating: 3,
+    text: 'The service needs improvement. Experience was below expectations.',
+  },
+  {
+    id: 7,
+    rating: 8,
+    text: 'Great service and experience overall, but some functionalities need fine-tuning.',
+  },
+  {
+    id: 8,
+    rating: 6,
+    text: 'Average experience. There is potential for improvement in several aspects.',
+  },
+  // Additional feedback can be added here
 ];
 
 export default FeedbackData;
