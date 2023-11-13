70c6c00541113fb41a15b833c5236710a8bcc7c1
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-13 17:49:15 -0500

    created a folder called pages for components that will be their own routes as well as skeleton for routing from tutorial. Will fix bugs from react router dom v5 to 6 in next commit

---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <div className="about">
+      <h1>About this Project</h1>
+      <p>This a react app to leave feedback for a product or service</p>
+      <p>1.0.0</p>
+      <p>
+        <a href="/">Back to home</a>
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
